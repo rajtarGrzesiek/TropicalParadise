3210d0ae3f7d3bd98d77974686db147eea628b89
Grzesiek <rajtar.grzesiek@gmail.com>
2024-06-01 09:57:25 +0200

    First commit. Added almost finished website from third part of frontend course.

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".burger-btn");
+const allNavItems = document.querySelectorAll(".nav__item");
+
+const handleNav = () => {
+	nav.classList.toggle("nav--active");
+
+	allNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
+const deleteAnimation = () => {
+	allNavItems.forEach((item) => {
+		item.classList.remove("nav-items-animation");
+	});
+};
+
+navBtn.addEventListener("click", handleNav);
